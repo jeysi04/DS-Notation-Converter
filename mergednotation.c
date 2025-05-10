dd2ed64cd7a992a36eb8a43aa4cbed527097c360
Jaycee Cadag <cadagjaycee37@gmail.com>
2025-05-10 14:16:17 +0800

    Add checker for malformed expressions.

--- mergednotation.c
+++ mergednotation.c
@@ -41,7 +41,7 @@ void printGuide(); //Prints detailed guide with explanations and examples
 int main(int argc, char *argv[]) {
     if(argc == 1){
         printHelp();
-    } else if(argc == 2 ){ //if argument is one
+    } else if(argc == 2){ //if argument is one
         // Validate argument for help or guide
         if (strcmp(argv[1], "-h") == 0 || strcmp(argv[1], "--help") == 0) {
             // Handle help flag
